71e7b4efbf35546ad1c0f996d1c0fdd0ae3c3641
Blake Pettersson <blake.pettersson@gmail.com>
2015-12-23 14:20:45 +0100

    Set aptible as administrator.

--- wrapper.sh
+++ wrapper.sh
@@ -16,14 +16,14 @@ fi
 #sed -i "s,CAFILE,$SSL_CA_FILE,g" ${RABBITMQ_HOME}/etc/rabbitmq/ssl.config
 
 if [[ "$1" == "--initialize" ]]; then
-#    mv ${RABBITMQ_HOME}/etc/rabbitmq/ssl.config \
-#       ${RABBITMQ_HOME}/etc/rabbitmq/rabbitmq.config
-
     ${RABBITMQ_HOME}/sbin/rabbitmq-server &
     sleep 25
     ${RABBITMQ_HOME}/sbin/rabbitmqctl add_user aptible $PASSPHRASE
     ${RABBITMQ_HOME}/sbin/rabbitmqctl add_vhost db
+
     ${RABBITMQ_HOME}/sbin/rabbitmqctl set_permissions -p db aptible ".*" ".*" ".*"
+    ${RABBITMQ_HOME}/sbin/rabbitmqctl set_user_tags aptible administrator
+
     ${RABBITMQ_HOME}/sbin/rabbitmqctl delete_user guest
     ${RABBITMQ_HOME}/sbin/rabbitmqctl delete_vhost /
 else

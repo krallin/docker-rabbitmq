10a600793d75e2f0ddb48e43225f7bb6d512357b
Blake Pettersson <blake.pettersson@gmail.com>
2016-01-13 18:27:00 +0100

    Add .travis.yml and travis deploy script

---
+++ script/travis-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Deploy to staging on master merges (not PRs)
+
+set -e
+
+# Don't deploy on PRs
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  # Deploy to staging on a merge to master
+  docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
+
+  #TODO: Once we actually have multiple version to support, we will need a strategy
+  #      to figure out how we will build multiple versions from different branches.
+  docker push quay.io/aptible/docker-rabbitmq:3.5
+fi
